b20aa7a6a054a1a8ce99793aad035ae3c5341a2b
Akivamu <akivamu@gmail.com>
2018-09-17 17:44:14 +0700

    PassportJS: Set token to axios

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -1,11 +1,23 @@
 import store from '../../store'
 import axios from 'axios'
 
+function setAuthInfo (authInfo) {
+  if (authInfo && authInfo.token_type && authInfo.access_token) {
+    axios.defaults.headers.common['Authorization'] = authInfo.token_type + ' ' + authInfo.access_token
+  } else {
+    delete axios.defaults.headers.common['Authorization']
+  }
+}
+
+// Load authInfo from store
+setAuthInfo(store.state.auth.authInfo)
+
 export default {
   login: function (username, password) {
     return axios.post('/auth/login', {username: username, password: password})
       .then(res => {
         store.commit('auth/setAuthInfo', res.data.data)
+        setAuthInfo(res.data.data)
       })
       .catch(error => {
         throw error.response.data
@@ -13,6 +25,7 @@ export default {
   },
   logout: function () {
     store.commit('auth/setAuthInfo', null)
+    setAuthInfo(null)
     return Promise.resolve()
   }
 }
